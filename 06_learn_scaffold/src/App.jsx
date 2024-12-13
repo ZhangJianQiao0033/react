4ed445ec62824e9dd3fe48b2fc048c44223b0588
zhangjianqiao <2497523984@qq.com>
2024-12-13 23:41:38 +0800

    06_learn_scaffold

---
+++ 06_learn_scaffold/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import HelloWorld from "./components/HelloWorld"
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state= {
+      message: "hello world"
+    }
+  }
+
+  render() {
+    const {message} = this.state
+    return (
+      <div>{message}
+      <HelloWorld/>
+      </div>
+      
+    )
+  }
+}
+
+export default App
