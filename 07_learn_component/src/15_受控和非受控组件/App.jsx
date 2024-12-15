8bc42c1f9168ac3625256950194c13a10fe84a31
zhangjianqiao <2497523984@qq.com>
2024-12-15 16:50:42 +0800

    受控和非受控组件

---
+++ 07_learn_component/src/15_受控和非受控组件/App.jsx
@@ -0,0 +1,120 @@
+import React, { PureComponent } from 'react'
+
+export class App extends PureComponent {
+
+  constructor() {
+    super()
+
+    this.state = {
+      username: "",
+      password: "",
+      isAgree: false,
+      hobbies:[
+        {value: "sing", text: "唱", isChecked: false},
+        {value: "dance", text: "跳", isChecked: false},
+        {value: "rap", text: "rap", isChecked: false}
+      ],
+      fruit: []
+    }
+  }
+
+  handleSubmitClick(event) {
+    // 1.阻止默认的行为
+    event.preventDefault()
+
+    // 2.获取到所有的表单数据, 对数据进行组件
+    console.log("获取所有的输入内容")
+    // console.log(this.state.username)
+    const hobbies = this.state.hobbies.filter(item => item.isChecked)
+    console.log(hobbies);
+    // 3.以网络请求的方式, 将数据传递给服务器(ajax/fetch/axios)
+  }
+
+  handleInputChange(event) {
+    console.log(event.target.name,event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  handleAgreeChange(e) {
+    this.setState({
+      isAgree: e.target.checked
+    })
+  }
+  handleHobbyChange(e, index) {
+    const hobbies = [...this.state.hobbies]
+    hobbies[index].isChecked = e.target.checked
+    this.setState({
+      hobbies
+    })
+  }
+  handleFruitChange(e) {
+    const fruit = Array.from(e.target.selectedOptions, item => item.value)
+    console.log(fruit);
+    this.setState({
+      fruit
+    })
+  }
+  render() {
+    const { username, password, isAgree, hobbies, fruit } = this.state
+
+    return (
+      <div>
+        <form onSubmit={e => this.handleSubmitClick(e)}>
+          {/* 1.用户名和密码 */}
+          <label htmlFor="username">
+            用户: 
+            <input 
+              id='username' 
+              type="text" 
+              name='username' 
+              value={username} 
+              onChange={e => this.handleInputChange(e)}
+            />
+          </label>
+          <label htmlFor="password">
+            用户: 
+            <input 
+              id='password' 
+              type="password" 
+              name='password' 
+              value={password} 
+              onChange={e => this.handleInputChange(e)}
+            />
+          </label>
+
+          {/* 2. checkbox */}
+          <label htmlFor="agree">
+            <input id="agree" type="checkbox" checked={isAgree} onChange={e => this.handleAgreeChange(e)} /> 注册协议
+          </label>
+
+          {/* 3.checkbox多选 */}
+          <div>
+            您的爱好
+          {
+              hobbies.map((item, index) => {
+                return (
+                  <label htmlFor={item.value} key={item.value}>
+                    <input id={item.value} type="checkbox" checked={item.isChecked} onChange={e => this.handleHobbyChange(e, index)} />
+                    <span>{item.text}</span> 
+                  </label>
+                  
+                )
+              })
+            }
+          </div>
+             {/* 4.select */}
+          <select value={fruit} onChange={e => this.handleFruitChange(e)} multiple>
+            <option value="apple">苹果</option>
+            <option value="orange">橘子</option>
+            <option value="banana">香蕉</option>
+          </select>
+          
+          <button type='submit'>注册</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
--- 07_learn_component/src/index.js
+++ 07_learn_component/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './14_ref获取DOM操作/03_ref获取函数式组件.jsx';
+import App from './15_受控和非受控组件/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
