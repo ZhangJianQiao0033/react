a159b47a03423356ec77cf6c46b26525a61c7bf0
zhangjianqiao <2497523984@qq.com>
2024-12-18 09:43:50 +0800

    09_learn_redux

--- 07_learn_component/src/index.js
+++ 07_learn_component/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './20_react动画实现/02_SwitchTransition/App';
+import App from './20_react动画实现//03_TransitionGroup/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ 09_learn_redux/src/store/index.js
@@ -0,0 +1,11 @@
+const { createStore } = require("redux")
+const reducer = require('./reducer.js')
+
+
+
+
+// 创建的store
+const store = createStore(reducer)
+
+module.exports = store
+
